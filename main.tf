3d822b04ae3b27c6ecb13e3f773453293ff39060
Nicolas Abihaggle <rabihaggle@gmail.com>
2023-04-06 15:54:52 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_website_configuration" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+}
