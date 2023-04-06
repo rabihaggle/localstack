e52e6f02b0d797c3c56f109e6ea666356f226e34
Nicolas Abihaggle <rabihaggle@gmail.com>
2023-04-06 16:03:48 +0200

    cleancode

--- main.tf
+++ main.tf
@@ -2,15 +2,4 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_website_configuration" "s3_bucket" {
-  bucket = aws_s3_bucket.s3_bucket.id
 
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-
-}
